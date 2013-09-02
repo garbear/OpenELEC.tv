ac6e28a27c16f8cea13f6711ec739eeddb925641
Garrett Brown <garbearucla@gmail.com>
2013-09-02 03:51:21 -0700

    RetroPlayer: update to 2870c6a (https://github.com/garbear/xbmc/commit/2870c6a)

--- packages/mediacenter/xbmc/package.mk
+++ packages/mediacenter/xbmc/package.mk
@@ -25,7 +25,7 @@ if [ "$XBMC" = "master" ]; then
 elif [ "$XBMC" = "xbmc-aml" ]; then
   PKG_VERSION="aml-frodo-d9119f2"
 elif [ "$XBMC" = "retroplayer" ]; then
-  PKG_VERSION="retroplayer-a17632b"
+  PKG_VERSION="retroplayer-2870c6a"
 fi
 PKG_REV="1"
 PKG_ARCH="any"
